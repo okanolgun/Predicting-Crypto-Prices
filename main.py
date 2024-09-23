2a75a20e7e6e11aa8a1c30a5c831931ff659a50a
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-09-24 00:20:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -22,4 +22,22 @@ data = yf.download(f'{crypto_currency}-{against_currency}', start=start, end=end
 
 
 # preparing data
-print(data.head())
+# print(data.head())
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+# we'll predict the price with the last 60 days
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x - prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+# we are going to use the time period from x - predictions days
+# up until x to predict the x, the actual x
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
